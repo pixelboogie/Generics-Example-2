65aae29b01d7806137128b6f15ec2b9e05bf489f
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-16 10:16:30 -0700

    Add files via upload

---
+++ TestingGenerics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingGenerics : MonoBehaviour
+{
+   
+    void Start()
+    {
+        TestMultiGenerics<int,string>(56, "asdf");
+    }
+
+
+    private void TestMultiGenerics<T1, T2>(T1 t1, T2 t2)
+    {
+        Debug.Log("t1: " + t1 + ", " + t1.GetType());
+        Debug.Log("t2: " + t2 + ", " +t2.GetType());
+    }
+
+}
